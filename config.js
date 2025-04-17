c55b456beb7b9eb09d95c817f06bc4c2ac0f532d
Julio Rodriguez <jircdev@gmail.com>
2025-04-16 21:47:02 -0300

    feat: version 0.1.6-dev.03

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/platform',
-				version: '0.1.6-dev.02',
+				version: '0.1.6-dev.03',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es']
@@ -15,7 +15,7 @@ System.register([], function (_export, _context) {
 				'global.css': true,
 				params: {
 					project: 'betterAgile',
-					api: {
+					apis: {
 						chat: 'https://dev.agents.api.aimpact.partners',
 						ailearn: 'https://dev.ailearn.api.aimpact.partners',
 						betterAgile: 'https://agents-api-883367315651.europe-west10.run.app'
