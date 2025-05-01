1067ee21e65e92079742390c253d41dae4c25496
Julio Rodriguez <jircdev@gmail.com>
2025-05-01 13:26:58 -0300

    feat: version 0.1.6-dev.14

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/platform',
-				version: '0.1.6-dev.13',
+				version: '0.1.6-dev.14',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es']
